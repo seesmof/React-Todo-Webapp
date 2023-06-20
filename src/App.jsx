d87aedbf781c69499f139487379d9c46033d4c9e
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-20 14:11:52 +0300

    feat: add the general structure of the website

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-stone-50 text-stone-900 flex container max-w-4xl p-4 mx-auto flex-col justify-between w-full">
-        <section className="flex flex-1 flex-col gap-2 w-full "></section>
-        <section className="flex flex-col w-full">
-          <h2 className="uppercase font-medium mb-2">search</h2>
-          <input
-            type="text"
-            className="input-field"
-            placeholder="Enter your search item..."
-          />
-        </section>
+      <div className="h-screen bg-stone-50 text-stone-900">
+        <div className="flex container max-w-4xl h-full p-4 lg:p-6 mx-auto flex-col lg:flex-col-reverse justify-between w-full">
+          <section className="flex flex-1 flex-col gap-2 w-full"></section>
+          <section className="flex flex-col w-full">
+            <h2 className="uppercase font-medium mb-1 lg:mb-2">add new</h2>
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter your search item..."
+            />
+          </section>
+        </div>
       </div>
     </>
   );
