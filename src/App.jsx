92e8dc45b0023c2e8d6509b466428e0aa24c8c43
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-20 14:16:20 +0300

    fix: fix section name, fix input placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App() {
         <div className="flex container max-w-4xl h-full p-4 lg:p-6 mx-auto flex-col lg:flex-col-reverse justify-between w-full">
           <section className="flex flex-1 flex-col gap-2 w-full"></section>
           <section className="flex flex-col w-full">
-            <h2 className="uppercase font-medium mb-1 lg:mb-2">add new</h2>
+            <h2 className="uppercase font-medium mb-1 lg:mb-2">add new todo</h2>
             <div className="flex flex-row gap-2">
               <input
                 type="text"
                 className="input-field"
-                placeholder="Enter your search item..."
+                placeholder="Enter your task..."
               />
               <button className="bg-stone-800 duration-300 hover:bg-stone-700 active:scale-95 active:bg-stone-700 px-6 rounded-lg font-medium text-white capitalize">
                 add
