b8dc531532dc1dc742e12ecd5c2775a39176b29d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-20 14:06:31 +0300

    chore: change site name

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="min-h-screen bg-stone-50 text-stone-900 flex container max-w-4xl p-4 mx-auto flex-col justify-between w-full">
+        <section className="flex flex-1 flex-col gap-2 w-full "></section>
+        <section className="flex flex-col w-full">
+          <h2 className="uppercase font-medium mb-2">search</h2>
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Enter your search item..."
+          />
+        </section>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
