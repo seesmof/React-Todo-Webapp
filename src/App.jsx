c419d3809151840869a980888ce117433d94b2b1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-20 14:15:49 +0300

    feat: add submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
       <div className="h-screen bg-stone-50 text-stone-900">
@@ -8,11 +10,16 @@ function App() {
           <section className="flex flex-1 flex-col gap-2 w-full"></section>
           <section className="flex flex-col w-full">
             <h2 className="uppercase font-medium mb-1 lg:mb-2">add new</h2>
-            <input
-              type="text"
-              className="input-field"
-              placeholder="Enter your search item..."
-            />
+            <div className="flex flex-row gap-2">
+              <input
+                type="text"
+                className="input-field"
+                placeholder="Enter your search item..."
+              />
+              <button className="bg-stone-800 duration-300 hover:bg-stone-700 active:scale-95 active:bg-stone-700 px-6 rounded-lg font-medium text-white capitalize">
+                add
+              </button>
+            </div>
           </section>
         </div>
       </div>
